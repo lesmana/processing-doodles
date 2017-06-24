92ab60edb84e0b50f30d7875147e7ced50dd8c75
Lesmana Zimmer <lesmana@gmx.de>
2017-06-24 23:30:18 +0200

    add message for humans

--- export.sh
+++ export.sh
@@ -1,6 +1,10 @@
 #! /bin/sh
 
-set -x
-rm -rf export
-processing-java --sketch=doodles --output=export --build
+(
+  set -x
+  rm -rf export
+  processing-java --sketch=doodles --output=export --build
+)
+echo "exported sketch now in directory export"
+echo "java source file in export/source/doodles.java"
 
