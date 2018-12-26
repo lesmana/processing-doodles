1996349924cfd90d552f6c88f5dd16c72e060d8f
Lesmana Zimmer <lesmana@gmx.de>
2018-12-26 15:32:54 +0100

    improve edit helper

--- edit.sh
+++ edit.sh
@@ -7,11 +7,26 @@ if [ $# != 1 ]; then
   exit 1
 fi
 
-if ! [ -e "$1" ]; then
-  echo "name \"$1\" does not exists"
+if [ -d "$1" ]; then
+  n="${1%/}"
+  if [ -f "$n/$n.pde" ]; then
+    processing "$n/$n.pde"
+  else
+    echo "not sketch dir"
+    exit 1
+  fi
+elif [ -f "$1" ]; then
+  d=$(dirname "$1")
+  f=$(basename "$1" .pde)
+  if [ "$d" == "$f" ]; then
+    processing "$1"
+  else
+    echo "not valid processing sketch dir and file name"
+    exit 1
+  fi
+else
+  echo "neither sketch dir nor pde file"
   exit 1
 fi
-
-processing "$1/$1.pde"
 
 trap - EXIT
