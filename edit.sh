ad6f3cd7181987deda8a5cee10463f1c386852c7
Lesmana Zimmer <lesmana@gmx.de>
2019-01-02 06:03:35 +0100

    better edit helper

--- edit.sh
+++ edit.sh
@@ -1,32 +1,46 @@
-#! /bin/sh
+#! /bin/bash
 
 trap 'echo error exit' EXIT
 
 if [ $# != 1 ]; then
-  echo "need argument: sketchname"
+  echo "need argument: name-of-processing-sketch-dir-or-file"
+  echo "will open processing editor with sketch"
+  echo "name can be:"
+  echo "  sketchname"
+  echo "  sketchname/"
+  echo "  sketchname/sketchname.pde"
   exit 1
 fi
 
+echo "given sketchname: $1"
+
+# make sure sketchname is valid processing sketch dir or file
 if [ -d "$1" ]; then
+  # if given dir name
   n="${1%/}"
   if [ -f "$n/$n.pde" ]; then
-    processing "$n/$n.pde"
+    sketchfilename="$n/$n.pde"
   else
-    echo "not sketch dir"
+    echo "sketchname is not valid processing sketch dir"
     exit 1
   fi
 elif [ -f "$1" ]; then
+  # if given file name
   d=$(dirname "$1")
   f=$(basename "$1" .pde)
   if [ "$d" == "$f" ]; then
-    processing "$1"
+    sketchfilename="$1"
   else
-    echo "not valid processing sketch dir and file name"
+    echo "sketchname is not valid processing sketch dir"
     exit 1
   fi
 else
-  echo "neither sketch dir nor pde file"
+  echo "sketchname is neither sketch dir nor pde file"
   exit 1
 fi
+
+echo "sketchfilename: $sketchfilename"
+
+echo processing "$sketchfilename"
 
 trap - EXIT
