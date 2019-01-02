3ba03390f3c7e038712a05bb59bfbf273ae87fe4
Lesmana Zimmer <lesmana@gmx.de>
2019-01-02 06:27:57 +0100

    advanced new helper

--- new.sh
+++ new.sh
@@ -1,18 +1,46 @@
-#! /bin/sh
+#! /bin/bash
 
 trap 'echo error exit' EXIT
 
 if [ $# != 1 ]; then
   echo "need argument: sketchname"
+  echo "will create sketch dir with sketch file"
+  echo "obeying the processing sketch rules"
+  echo "that dirname and filename must be same"
+  echo "for example sketchname/sketchname.pde"
+  echo "sketchname may be:"
+  echo "  somename"
+  echo "  somename/"
+  echo "  somename/somename.pde"
+  echo "  somename.pde"
   exit 1
 fi
+
+echo "given sketchname: $1"
 
 if [ -e "$1" ]; then
-  echo "name \"$1\" already exists"
+  echo "sketchname already exists"
   exit 1
 fi
 
-mkdir "$1"
-touch "$1/$1.pde"
+d=$(dirname "$1")
+f=$(basename "$1" .pde)
+if [ "$d" = "." ]; then
+  # given only dir name or only file name
+  sketchdir="$f"
+  sketchfile="$f".pde
+elif [ "$d" == "$f" ]; then
+  # given dir name file name combo
+  sketchdir="$d"
+  sketchfile="$f".pde
+else
+  echo "sketchname is not valid processing sketch dir"
+  exit 1
+fi
+
+echo "will create: $sketchdir/$sketchfile"
+
+echo mkdir "$sketchdir"
+echo touch "$sketchdir/$sketchfile"
 
 trap - EXIT
